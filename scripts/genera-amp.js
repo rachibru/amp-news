68e581e517e0188b4c17cc55faf68d3517ba1ed4
rachibru <73406615+rachibru@users.noreply.github.com>
2026-01-11 14:43:24 +0100

    Update genera-amp.js

--- scripts/genera-amp.js
+++ scripts/genera-amp.js
@@ -1,72 +1,23 @@
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
+// Per ogni articolo
+for (const item of items) {
+  // Creiamo uno slug semplice senza doppie estensioni
+  let slug = item.link.replace(/^https?:\/\/[^\/]+\/|\/$/g, ''); // esempio: politica-2026-analisi-demoscopiche-scenari
 
-// Cartelle output
-const ampDir = 'amp';
-const outDir = 'output';
+  // Se vuoi cartelle per anno/mese:
+  const pub = new Date(item.pubDate);
+  const folder = path.join(ampDir, pub.getFullYear().toString(), String(pub.getMonth()+1).padStart(2,'0'));
 
-// Crea cartelle se non esistono
-if (!fs.existsSync(ampDir)) fs.mkdirSync(ampDir);
-if (!fs.existsSync(outDir)) fs.mkdirSync(outDir);
+  // CREA CARTELLE SE NON ESISTONO
+  fs.mkdirSync(folder, { recursive: true });
 
-// URL del feed RSS
-const RSS_URL = 'https://feeds.feedburner.com/brunorachiele/ZOU113SCMgV';
+  const filePath = path.join(folder, slug + '.html'); // <-- così non ci saranno .html.html
 
-// Funzione per leggere RSS
-function fetchRSS(url) {
-  return new Promise((resolve, reject) => {
-    https.get(url, res => {
-      let data = '';
-      res.on('data', chunk => data += chunk);
-      res.on('end', () => resolve(data));
-    }).on('error', err => reject(err));
-  });
+  let html = fs.readFileSync('article.html', 'utf8');
+  html = html.replace(/{{title}}/g, item.title)
+             .replace(/{{link}}/g, item.link)
+             .replace(/{{pubDate}}/g, item.pubDate)
+             .replace(/{{isoDate}}/g, new Date(item.pubDate).toISOString());
+
+  fs.writeFileSync(filePath, html, 'utf8');
+  console.log('Generata pagina AMP:', filePath);
 }
-
-fetchRSS(RSS_URL).then(xml => {
-  const items = [];
-  xml.replace(/<item>([\s\S]*?)<\/item>/g, (_, block) => {
-    const title = /<title>(.*?)<\/title>/.exec(block);
-    const link = /<link>(.*?)<\/link>/.exec(block);
-    const pubDate = /<pubDate>(.*?)<\/pubDate>/.exec(block);
-    if (title && link && pubDate) items.push({ title: title[1], link: link[1], pubDate: pubDate[1] });
-  });
-
-  // Genera pagine AMP
-  for (const item of items) {
-    const slug = item.link.replace(/^https?:\/\/[^\/]+\/|\/$/g, '');
-    const filePath = path.join(ampDir, slug + '.html');
-    let html = fs.readFileSync('article.html', 'utf8');
-    html = html.replace(/{{title}}/g, item.title)
-               .replace(/{{link}}/g, item.link)
-               .replace(/{{pubDate}}/g, item.pubDate)
-               .replace(/{{isoDate}}/g, new Date(item.pubDate).toISOString());
-    fs.writeFileSync(filePath, html, 'utf8');
-    console.log('Generata pagina AMP:', filePath);
-  }
-
-  // Genera sitemap.xml
-  let sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
-  for (const item of items) {
-    const slug = item.link.replace(/^https?:\/\/[^\/]+\/|\/$/g, '');
-    sitemap += `  <url>\n    <loc>https://amp.brunorachiele.it/${slug}.html</loc>\n    <lastmod>${new Date(item.pubDate).toISOString()}</lastmod>\n  </url>\n`;
-  }
-  sitemap += '</urlset>';
-  fs.writeFileSync(path.join(outDir, 'sitemap.xml'), sitemap, 'utf8');
-  console.log('Generata sitemap.xml');
-
-  // Genera mini-index ultimi articoli
-  let indexHtml = '<ul>\n';
-  for (const item of items) {
-    const slug = item.link.replace(/^https?:\/\/[^\/]+\/|\/$/g, '');
-    indexHtml += `  <li><a href="https://amp.brunorachiele.it/${slug}.html">${item.title}</a></li>\n`;
-  }
-  indexHtml += '</ul>';
-  fs.writeFileSync(path.join(outDir, 'ultimi.html'), indexHtml, 'utf8');
-  console.log('Generato mini-index ultimi articoli');
-
-}).catch(err => {
-  console.error('Errore nella generazione AMP o sitemap:', err);
-  process.exit(1);
-});
