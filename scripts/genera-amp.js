fc73566fc2215cdef1efde33fdbee1f03b3db77a
rachibru <73406615+rachibru@users.noreply.github.com>
2026-01-11 13:02:55 +0100

    Update genera-amp.js

--- scripts/genera-amp.js
+++ scripts/genera-amp.js
@@ -10,7 +10,7 @@ const cheerio = require('cheerio');
 const ampDir = 'amp';
 fs.ensureDirSync(ampDir);
 
-// Il tuo feed RSS
+// URL del feed RSS
 const RSS_URL = 'https://feeds.feedburner.com/brunorachiele/ZOU113SCMgV';
 
 async function main() {
@@ -24,7 +24,6 @@ async function main() {
     const items = result.rss.channel[0].item;
 
     const posts = items.map(item => {
-      // crea uno slug dall'URL
       const slug = item.link[0].replace(/^https?:\/\/[^\/]+\/|\/$/g, '');
       const title = item.title[0];
       const date = dayjs(item.pubDate[0]).format('YYYY-MM-DD');
@@ -32,46 +31,50 @@ async function main() {
       return { slug, title, date, content };
     });
 
-    // --- Genera file AMP ---
+    // --- Genera file AMP con cartelle anno/mese ---
     posts.forEach(post => {
+      const pubDate = dayjs(post.date);
+      const folderPath = path.join(ampDir, pubDate.format('YYYY'), pubDate.format('MM'));
+      fs.ensureDirSync(folderPath); // crea tutte le cartelle intermedie
+
+      const filePath = path.join(folderPath, `${post.slug}.html`);
       const html = `<!doctype html>
 <html amp lang="it">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
   <title>${post.title}</title>
-  <link rel="canonical" href="https://amp.brunorachiele.it/${post.slug}.html">
+  <link rel="canonical" href="https://amp.brunorachiele.it/${pubDate.format('YYYY')}/${pubDate.format('MM')}/${post.slug}.html">
   <meta name="pubdate" content="${post.date}">
   <script async src="https://cdn.ampproject.org/v0.js"></script>
 </head>
 <body>
   <h1>${post.title}</h1>
-  <time datetime="${post.date}">${dayjs(post.date).format('D MMMM YYYY')}</time>
+  <time datetime="${post.date}">${pubDate.format('D MMMM YYYY')}</time>
   ${post.content}
 </body>
 </html>`;
 
-      const filePath = path.join(ampDir, `${post.slug}.html`);
       fs.writeFileSync(filePath, html, 'utf8');
       console.log(`Generata pagina AMP: ${filePath}`);
     });
 
     // --- Genera sitemap ---
-    const pages = posts.map(post => ({
-      file: `${post.slug}.html`,
-      title: post.title,
-      pubdate: post.date
-    }));
+    const pages = posts.map(post => {
+      const pubDate = dayjs(post.date);
+      const file = path.join(ampDir, pubDate.format('YYYY'), pubDate.format('MM'), `${post.slug}.html`);
+      return { file, title: post.title, pubdate: post.date };
+    });
 
-    // Ordina per data decrescente
     pages.sort((a,b) => new Date(b.pubdate) - new Date(a.pubdate));
 
     const urlset = builder.create('urlset', { encoding: 'UTF-8' });
     urlset.att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
 
     pages.forEach(page => {
+      const pathUrl = path.relative(ampDir, page.file).replace(/\\/g, '/');
       urlset.ele('url')
-        .ele('loc', {}, `https://amp.brunorachiele.it/${page.file}`).up()
+        .ele('loc', {}, `https://amp.brunorachiele.it/${pathUrl}`).up()
         .ele('lastmod', {}, page.pubdate).up()
         .ele('title', {}, page.title);
     });
